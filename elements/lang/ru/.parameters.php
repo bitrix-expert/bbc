0d05eaee198da98a9a284d8c757a6d596daf2cb2
Evgeny Mikulich <sidpunk@inbox.ru>
2015-12-31 17:08:01 +0500

    Add support complex component variables SECTION_CODE_PATH and SMART_FILTER_PATH

--- elements/lang/ru/.parameters.php
+++ elements/lang/ru/.parameters.php
@@ -6,4 +6,5 @@ $MESS['ELEMENTS_GROUP_DETAIL'] = 'Страница элемента';
 $MESS['ELEMENTS_SEF_INDEX'] = 'Главная страница';
 $MESS['ELEMENTS_SEF_SECTION'] = 'Раздел';
 $MESS['ELEMENTS_SEF_DETAIL'] = 'Страница элемента';
+$MESS['ELEMENTS_SEF_SMART_FILTER'] = 'Страница раздела для умного фильтра';
 $MESS['ELEMENTS_DETAIL_PARAMETERS_USE_SEARCH'] = 'Использовать поиск';
